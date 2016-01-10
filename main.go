dc4f290831e1a94e240439bd80179911bf589746
Gosuke Miyashita <gosukenator@gmail.com>
2016-01-11 01:55:01 +0900

    Temporary commit

--- main.go
+++ main.go
@@ -8,16 +8,25 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"os/exec"
+	"strconv"
 	"time"
 
 	"github.com/walter-cd/walter-server/api"
 )
 
 var server string
+var maxWorkers int64
+var workers int64 = 0
+var interval int64
+var workingDir string
 
 func main() {
 	flags := flag.NewFlagSet("walter-agent", flag.ExitOnError)
 	flags.StringVar(&server, "server", "http://localhost:8080/", "URL of walter-server")
+	flags.Int64Var(&maxWorkers, "max-workers", 5, "Maximum number of walter workers")
+	flags.Int64Var(&interval, "internal", 1, "Job polling interval by seconds")
+	flags.StringVar(&workingDir, "working-dir", "/var/tmp/walter", "Working directory")
 
 	if err := flags.Parse(os.Args[1:]); err != nil {
 		panic(err)
@@ -32,20 +41,25 @@ func main() {
 
 func pollJob(queue chan api.Job) {
 	for {
-		res, _ := http.Get(fmt.Sprintf("%s/api/v1/jobs/pop", server))
-		fmt.Printf("%s\n", res.Status)
+		time.Sleep(time.Duration(interval) * time.Second)
 
-		rb := bufio.NewReader(res.Body)
-		var body string
-		for {
-			s, err := rb.ReadString('\n')
-			body = body + s
-			if err == io.EOF {
-				break
-			}
+		if workers >= maxWorkers {
+			continue
 		}
 
+		res, _ := http.Get(fmt.Sprintf("%s/api/v1/jobs/pop", server))
+
 		if res.Status == "200 OK" {
+			rb := bufio.NewReader(res.Body)
+			var body string
+			for {
+				s, err := rb.ReadString('\n')
+				body = body + s
+				if err == io.EOF {
+					break
+				}
+			}
+
 			var job api.Job
 			err := json.Unmarshal([]byte(body), &job)
 			if err != nil {
@@ -53,34 +67,37 @@ func pollJob(queue chan api.Job) {
 			}
 			queue <- job
 		}
-		time.Sleep(1 * 1000000000)
 	}
 }
 
 func processJob(queue chan api.Job) {
-	maxJobCount := 5
-	jobCount := 0
 	done := make(chan bool)
 
 	for {
-		fmt.Println("Waiting job ...")
-		if jobCount < maxJobCount {
-			job := <-queue
-			jobCount++
-			go runWalter(job, done)
-		}
-
 		select {
+		case job := <-queue:
+			go runWalter(job, done, workers)
+			workers++
 		case <-done:
-			jobCount--
-		default:
-			time.Sleep(1 * 1000000000)
+			workers--
 		}
 	}
 }
 
-func runWalter(job api.Job, done chan bool) {
-	fmt.Printf("%#v\n", job)
-	time.Sleep(100 * 1000000000)
+func runWalter(job api.Job, done chan bool, num int64) {
+	workerDir := workingDir + "/" + strconv.FormatInt(num, 10)
+	if err := os.MkdirAll(workerDir, 0755); err != nil {
+		panic(err)
+	}
+
+	repoDir := workerDir + "/" + job.Project
+	if err := os.MkdirAll(repoDir, 0755); err != nil {
+		panic(err)
+	}
+
+	exec.Command("git", "clone", job.CloneUrl, repoDir).CombinedOutput()
+	os.Chdir(repoDir)
+	exec.Command("git", "checkout", job.Revision).CombinedOutput()
+
 	done <- true
 }
