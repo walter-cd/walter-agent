faa20998dc82e205a393e194c8ced5650bea1bcf
Gosuke Miyashita <gosukenator@gmail.com>
2016-01-13 03:23:56 +0900

    Update

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"container/list"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -13,6 +14,9 @@ import (
 	"time"
 
 	"github.com/walter-cd/walter-server/api"
+	"github.com/walter-cd/walter/config"
+	"github.com/walter-cd/walter/stages"
+	"github.com/walter-cd/walter/walter"
 )
 
 var server string
@@ -24,9 +28,9 @@ var workingDir string
 func main() {
 	flags := flag.NewFlagSet("walter-agent", flag.ExitOnError)
 	flags.StringVar(&server, "server", "http://localhost:8080/", "URL of walter-server")
-	flags.Int64Var(&maxWorkers, "max-workers", 5, "Maximum number of walter workers")
+	flags.Int64Var(&maxWorkers, "max_workers", 5, "Maximum number of walter workers")
 	flags.Int64Var(&interval, "internal", 1, "Job polling interval by seconds")
-	flags.StringVar(&workingDir, "working-dir", "/var/tmp/walter", "Working directory")
+	flags.StringVar(&workingDir, "working_dir", "/var/tmp/walter", "Working directory")
 
 	if err := flags.Parse(os.Args[1:]); err != nil {
 		panic(err)
@@ -95,9 +99,119 @@ func runWalter(job api.Job, done chan bool, num int64) {
 		panic(err)
 	}
 
-	exec.Command("git", "clone", job.CloneUrl, repoDir).CombinedOutput()
+	out, err := exec.Command("git", "clone", job.CloneUrl, repoDir).CombinedOutput()
+	fmt.Println(string(out))
+	fmt.Println(err)
+
 	os.Chdir(repoDir)
-	exec.Command("git", "checkout", job.Revision).CombinedOutput()
+
+	out, err = exec.Command("git", "fetch").CombinedOutput()
+	fmt.Println(string(out))
+	fmt.Println(err)
+
+	out, err = exec.Command("git", "checkout", job.Revision).CombinedOutput()
+	fmt.Println(string(out))
+	fmt.Println(err)
+
+	opts := &config.Opts{
+		PipelineFilePath: "./pipeline.yml",
+		Mode:             "local",
+	}
+
+	w, _ := walter.New(opts)
+	start := time.Now().Unix()
+	result := w.Run()
+	end := time.Now().Unix()
+
+	postReport(job, result, w, start, end)
 
 	done <- true
+}
+
+func postReport(job api.Job, result bool, w *walter.Walter, start int64, end int64) {
+	var status string
+	if result {
+		status = "success"
+	} else {
+		status = "fail"
+	}
+
+	report := &api.Report{
+		Project: &api.Project{
+			Name: job.Project,
+			Repo: job.HtmlUrl,
+		},
+		Status:     status,
+		Branch:     job.Branch,
+		CompareUrl: job.CompareUrl,
+		TriggeredBy: &api.User{
+			Name:      job.TriggeredBy.Name,
+			Url:       job.TriggeredBy.Url,
+			AvatarUrl: job.TriggeredBy.AvatarUrl,
+		},
+		Start: start,
+		End:   end,
+	}
+
+	for _, commit := range job.Commits {
+		report.Commits = append(report.Commits, &api.Commit{
+			Revision: commit.Revision,
+			Author:   commit.Author,
+			Message:  commit.Message,
+			Url:      commit.Url,
+		})
+	}
+
+	for s := w.Engine.Resources.Pipeline.Stages.Front(); s != nil; s = s.Next() {
+		stage := s.Value.(*stages.CommandStage)
+
+		var status string
+		if stage.GetReturnValue() {
+			status = "success"
+		} else {
+			status = "fail"
+		}
+
+		report.Stages = append(report.Stages, &api.Stage{
+			Name:   stage.StageName,
+			Status: status,
+			Stages: getChildStages(stage.ChildStages),
+			Out:    stage.OutResult,
+			Err:    stage.ErrResult,
+			Start:  stage.Start,
+			End:    stage.End,
+		})
+	}
+
+	b, _ := json.Marshal(report)
+	fmt.Println(string(b))
+}
+
+func getChildStages(l list.List) (st []*api.Stage) {
+	for s := l.Front(); s != nil; s = s.Next() {
+
+		stage := s.Value.(*stages.CommandStage)
+
+		var status string
+		if stage.GetReturnValue() {
+			status = "success"
+		} else {
+			status = "fail"
+		}
+
+		st = append(st, &api.Stage{
+			Name:   stage.StageName,
+			Status: status,
+			Stages: getChildStages(stage.ChildStages),
+			Out:    stage.OutResult,
+			Err:    stage.ErrResult,
+			Start:  stage.Start,
+			End:    stage.End,
+		})
+	}
+
+	return
+}
+
+func updateStatus() {
 }
